0c0869dcfc57e82829f3c47d326d35fd9cfc5185
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-05-16 18:43:55 -0300

    Requisito 2 ok

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,16 @@ routes.get('/talker', (request, response, next) => {
   next();
 });
 
+routes.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+
+  const user = readContent('talker.json').filter((u) => u.id === parseInt(id, 10));
+
+  if (user.length === 0) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  res.status(200).json(user[0]);
+});
+
 module.exports = routes;
